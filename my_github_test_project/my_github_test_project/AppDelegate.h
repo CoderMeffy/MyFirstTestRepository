62cbbd177ca1b0c6b09e3ab6e7fc13d7c8d200b5
CoderMeffy <mengfly35472341@163.com>
2017-03-19 20:54:20 +0800

    初始化项目文件

---
+++ my_github_test_project/my_github_test_project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  my_github_test_project
+//
+//  Created by meffy on 2017/3/19.
+//  Copyright © 2017年 meffy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ my_github_test_project/my_github_test_project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  my_github_test_project
+//
+//  Created by meffy on 2017/3/19.
+//  Copyright © 2017年 meffy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
